419f802f1d3010a59914f26bf6ef3bfc6f1961bb
Maycon Andrade Santos <maycongamessantos@gmail.com>
2026-05-15 23:08:14 +0000

    versao final jogo de xadrez

--- Olamundo.c
+++ Olamundo.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 #include <locale.h>
 
+void loopmovertorre(int casas){
+if (casas >0)
+{
+    printf("direita\n");
+
+    loopmovertorre(casas - 1);
+}
+}
+
+void moverBispoRecursivo(int casas) {
+    if (casas <= 0) return; // 1. Condição de parada (Caso Base)
+
+    // 2. Loops Aninhados (Movimento por casa)
+    for (int v = 0; v < 1; v++) { // Loop externo (Vertical)
+        for (int h = 0; h < 1; h++) { // Loop interno (Horizontal)
+            printf("Cima\n");
+            printf("Direita\n");
+        }
+    }
+
+    moverBispoRecursivo(casas - 1); // 3. Chamada Recursiva
+}
+
+
+
+void loopmoverrainha(int casainicial){
+    if (casainicial <=0) return;
+    {
+        printf("esquerda\n");
+    }
+
+    loopmoverrainha(casainicial-1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    
 
+/*
+Cavalo	while + for	O while controla a execução principal, e o for repete o movimento vertical.
+Torre	while	Executa enquanto a condição for verdadeira (testa antes de rodar).
+Bispo	do-while	Garante que o bispo se mova pelo menos uma vez antes de checar a condição.
+Rainha	for	Ideal para quando você já sabe exatamente o limite (8 casas).
+*/
 
 int escolha;
 
@@ -13,7 +68,7 @@ printf("digite 3 se quer jogar a torre\n");
 printf("digite 4 se quer jogar o bispo\n");
 printf("digite 5 se quer jogar a Rainha\n");
 printf("\n");
-scanf("%d", &escolha);
+scanf(" %d", &escolha);
 printf("\n");
 
 switch (escolha)
@@ -21,7 +76,7 @@ switch (escolha)
 
 case 1:
 {
-
+    
 int cavalo;
  cavalo=1;
     while (cavalo--)
@@ -57,44 +112,32 @@ case 2:
 }
     
 case 3:
-int torre =1;
-
-while (torre <=5)
 {
-    printf("a torre moveu %d vezes \n",torre);
-    printf("direita>\n");
-    torre++;
-    printf("\n");
-}
+    
+    loopmovertorre(5);
+
 break;
+}
 
 case 4:
-int bispo =1;
-
-
-do
 {
-    printf(" o bispo moveu %d vezes\n",bispo);
-    printf(" Cima,Direita\n");
-    bispo++;
-    printf("\n");
 
-} while (bispo <=5);
+    moverBispoRecursivo(5);
+
 break;
-
+}
 
 case 5:
-
-for (int rainha = 1; rainha<= 8; rainha++)
 {
-  printf("a rainha moveu %d vezes\n",rainha);  
-  printf("<esquerda\n");
-  printf("\n");
+    printf("a rainha moveu \n");
+    loopmoverrainha(8);
 
-}
+
 break;
+}
 
-default:{
+default:
+{
 
 printf("opção invalida\n");
 }
